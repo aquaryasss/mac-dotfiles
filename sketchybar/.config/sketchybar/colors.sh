7c5d1bba9c64ab3ffbca27152318fee6ba3d9e58
aquaryasss <nicoletereine@gmail.com>
2026-01-22 21:35:31 +0800

    Added tiling window manager

---
+++ sketchybar/.config/sketchybar/colors.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+export BLACK=0xff11111b
+export BASE=0xff1e1e2e
+export WHITE=0xffcdd6f4
+export GREY=0xff45475a
+export PINK=0xfff5c2e7
+export MAUVE=0xffcba6f7
+export BAR_COLOR=0xcc11111b
+export ACCENT_COLOR=$PINK
+export ITEM_BG_COLOR=$GREY
